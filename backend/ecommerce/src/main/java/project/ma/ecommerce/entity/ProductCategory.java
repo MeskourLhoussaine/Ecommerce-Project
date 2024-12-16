5b5ad04e3d1e5cbe9f6c4821ca771776bfe44847
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-12-16 10:58:20 +0100

    COMMIT

--- backend/ecommerce/src/main/java/project/ma/ecommerce/entity/ProductCategory.java
+++ backend/ecommerce/src/main/java/project/ma/ecommerce/entity/ProductCategory.java
@@ -22,6 +22,6 @@ public class ProductCategory {
     private String categoryName;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
-    private Set<Product> products;
+    private Set<Product> products; //ffff
 
 }
